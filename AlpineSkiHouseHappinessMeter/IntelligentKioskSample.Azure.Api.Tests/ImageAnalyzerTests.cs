3abba73ef799bb9a3ee9802507b5ed57743fcdac
Maximiliano Duva <maduva@microsoft.com>
2018-08-01 13:24:58 -0300

    Cleaned up version

---
+++ AlpineSkiHouseHappinessMeter/IntelligentKioskSample.Azure.Api.Tests/ImageAnalyzerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ServiceHelpers.Tests
+{
+    [TestClass]
+    public class ImageAnalyzerTests
+    {
+        [TestMethod]
+        public async Task FaceApi()
+        {
+            ImageAnalyzer ia = new ImageAnalyzer(File.ReadAllBytes(@"C:\Dev\intelligent-apps\ServiceHelpers.Tests\Assets\demo_face_api.png"));// "https://docs.microsoft.com/en-us/azure/cognitive-services/face/images/getting-started-cs-ui.png");
+
+            await ia.DetectEmotionAsync();
+
+            Assert.IsNotNull(ia.DetectedEmotion);
+        }
+
+        [TestMethod]
+        public async Task FaceApiEmotion()
+        {
+            ImageAnalyzer ia = new ImageAnalyzer(File.ReadAllBytes(@"C:\Dev\intelligent-apps\ServiceHelpers.Tests\Assets\demo_face_api.png"));// "https://docs.microsoft.com/en-us/azure/cognitive-services/face/images/getting-started-cs-ui.png");
+
+            await ia.DetectEmotionAsync();
+
+            Assert.IsNotNull(ia.DetectedEmotion);
+            Assert.IsTrue(ia.DetectedEmotion.Count > 0);
+        }
+
+        [TestMethod]
+        public async Task FaceApiEmotionScores()
+        {
+            ImageAnalyzer ia = new ImageAnalyzer(File.ReadAllBytes(@"C:\Dev\intelligent-apps\ServiceHelpers.Tests\Assets\demo_face_api.png"));// "https://docs.microsoft.com/en-us/azure/cognitive-services/face/images/getting-started-cs-ui.png");
+
+            await ia.DetectEmotionAsync();
+
+            Assert.IsNotNull(ia.DetectedEmotion);
+            Assert.IsTrue(ia.DetectedEmotion.Count > 0);
+            double emotionsSum = (from j in ia.DetectedEmotion select j.Scores).Sum(x => x.Anger + x.Contempt + x.Disgust + x.Fear + x.Happiness + x.Neutral + x.Sadness + x.Surprise);
+            Assert.IsTrue(emotionsSum > 0);
+        }
+    }
+}
---
+++ AlpineSkiHouseHappinessMeter/IntelligentKioskSample.Azure.Api.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntelligentKioskSample.Azure.Api.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntelligentKioskSample.Azure.Api.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("454487af-52f5-4c66-b605-99f819b40bbc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
