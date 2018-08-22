e6db0ac4ea97976480836b91014fff82fc2e2895
Leo Leong <leoleo@microsoft.com>
2018-08-21 18:03:07 -0700

    added Face library using statement

--- AlpineSkiHouseHappinessMeter/IntelligentKioskSample.Azure.Api/EmotionServiceHelper.cs
+++ AlpineSkiHouseHappinessMeter/IntelligentKioskSample.Azure.Api/EmotionServiceHelper.cs
@@ -31,6 +31,8 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // 
 
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 using ServiceHelpers.Data;
 using System;
 using System.Collections.Generic;
