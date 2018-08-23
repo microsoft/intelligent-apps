591f7378b11aee2b430ba18df477003e05c8998f
Shamlia Shukkur <shamlia@live.com>
2018-08-23 16:13:20 +0400

    Added validate method for validating field in Register Page

--- WoodgroveBankEnhancedATMSecurity/WoodgroveBankRegistration/Views/RegisterPage.xaml.cs
+++ WoodgroveBankEnhancedATMSecurity/WoodgroveBankRegistration/Views/RegisterPage.xaml.cs
@@ -138,50 +138,12 @@ namespace WoodgroveBankRegistration.Views
             //Disable the button
             Button_RegisterUser.IsEnabled = true;
         }
-
-        private async Task<bool> LogInAsync()
+        private bool Validate()
         {
-            //Encrypt the PIN
-            string password = CryptographicBuffer.EncodeToHexString(HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256).HashData(CryptographicBuffer.ConvertStringToBinary(PasswordBox_PIN.Password, BinaryStringEncoding.Utf8)));
-
-            //Invoke Log In method
-            var result = await storageService.SignInAsync(TextBox_Name.Text, password);
-
-            //If log in is successful
-            if (result is bool)
-            {
-                var username = TextBox_Name.Text.ToLower().Replace(" ", "");
-
-                //Get all persons in the person group
-                var plist = await faceClient.ListPersonsAsync(AppSettings.defaultPersonGroupID);
-                if (!(plist is bool))
-                {
-                    var personlist = plist as List<PersonDetails>;
-
-                    //Iterate through the persons in the person group to check if username matches
-                    foreach (var item in personlist)
-                    {
-                        if (item.userData == username)
-                        {
-                            //If a Person exists in Face API with the same username
-                            //then save person ID and username in local settings
-                            localSettings.Values["PersonId"] = item.personId;
-                            localSettings.Values["UserName"] = item.userData;
-
-                            //Navigate to Dashboard Page - Successful Log In
-                            Frame.Navigate(typeof(Dashboard));
-                        }
-                    }
-                }
-            }
-            else
-            {
-                //Display the failure message
-                msg.Title = "Invalid Log In!";
-                msg.Content = result.ToString();
-                await msg.ShowAsync();
+            if (PasswordBox_PIN.Password.Trim().Length == 0)
                 return false;
-            }
+            if (TextBox_Name.Text.Trim().Length == 0)
+                return false;
             return true;
         }
     }
