2217680959fa8155418d4a9ea45d422a9dc6e6b5
Shamlia Shukkur <shamlia@live.com>
2018-08-23 15:17:27 +0400

    Replaced baseuri with FaceAPIEndpoint to avoid confusion

--- WoodgroveBankEnhancedATMSecurity/WoodgrovePortable/Common/AppSettings.cs
+++ WoodgroveBankEnhancedATMSecurity/WoodgrovePortable/Common/AppSettings.cs
@@ -7,7 +7,7 @@ namespace WoodgrovePortable.Common
     public static class AppSettings
     {
         public static string FaceAPIKey = "<your API key here>";
-        public static string baseuri = "<provide base URI here>";
+        public static string FaceAPIEndpoint = "<provide base URI here>";
         public static string StorageAccountName = "<storage account name here>";
         public static string StorageAccountAccessKey = "<storage account access key here>";
         public static string defaultPersonGroupID = "demo1";
