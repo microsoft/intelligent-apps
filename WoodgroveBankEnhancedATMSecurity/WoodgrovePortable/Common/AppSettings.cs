4c2c072d4fec3fa7802b4fb28168863391934e7d
Shamlia Shukkur <shamlia@live.com>
2018-08-23 14:30:19 +0400

    changed API key to FaceAPIKey (to avoid confusion)

--- WoodgroveBankEnhancedATMSecurity/WoodgrovePortable/Common/AppSettings.cs
+++ WoodgroveBankEnhancedATMSecurity/WoodgrovePortable/Common/AppSettings.cs
@@ -6,7 +6,7 @@ namespace WoodgrovePortable.Common
 {
     public static class AppSettings
     {
-        public static string APIKEY = "<your API key here>";
+        public static string FaceAPIKey = "<your API key here>";
         public static string baseuri = "<provide base URI here>";
         public static string StorageAccountName = "<storage account name here>";
         public static string StorageAccountAccessKey = "<storage account access key here>";
