76c3e02542dae72ff634248589a48278d8c55dac
Wandrille Hubert <wahube@microsoft.com>
2018-08-31 16:43:53 -0500

    Revert "PBI3 End Updates"
    This reverts commit a39a84106383adeb3aaa494b730a5ba2e4bbb216.

--- FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.xaml.cs
+++ FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.xaml.cs
@@ -59,7 +59,7 @@ namespace CallFabrikamCustomerService
             //Initialize the speech end point & key from app.config
             MicrosoftSpeechToTextEndpoint = ConfigurationManager.AppSettings["MicrosoftSpeechToTextEndpoint"];
             MicrosoftSpeechApiKey = ConfigurationManager.AppSettings["MicrosoftSpeechApiKey"];
-            MicrosoftSpeechAccessTokenEndpoint = ConfigurationManager.AppSettings["MicrosoftAccessTokenEndpoint"];
+            MicrosoftSpeechAccessTokenEndpoint = ConfigurationManager.AppSettings["MicrosoftSpeechAccessTokenEndpoint"];
             MicrosoftTextToSpeechEndpoint = ConfigurationManager.AppSettings["MicrosoftTextToSpeechEndpoint"];
             Region = "westus";
 
@@ -226,9 +226,6 @@ namespace CallFabrikamCustomerService
 
                 //Play audio from text to speech API
                 await PlaySpeechAudioAsync(result);
-
-                //Start Microphone
-                StartMicrophone();
             }
         }
 
