9a811035700fa7d8599ec52162d471d0a645337b
wandrille-hubert <wahube@mcirosoft.com>
2018-09-04 09:54:41 -0700

    pbi3begin fabrikam updates

--- FabrikamInvestmentCustomerService/FabrikamCustomerServiceBot/Global.asax.cs
+++ FabrikamInvestmentCustomerService/FabrikamCustomerServiceBot/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Microsoft.Bot.Builder.Azure;
 
 namespace FabrikamCustomerServiceBot
 {
@@ -11,6 +12,24 @@ namespace FabrikamCustomerServiceBot
     {
         protected void Application_Start()
         {
+            var store = new InMemoryDataStore();
+
+            Conversation.UpdateContainer(
+                       builder =>
+                       {
+                           builder.Register(c => store)
+                                     .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                     .AsSelf()
+                                     .SingleInstance();
+
+                           builder.Register(c => new CachingBotDataStore(store,
+                                      CachingBotDataStoreConsistencyPolicy
+                                      .ETagBasedConsistency))
+                                      .As<IBotDataStore<BotData>>()
+                                      .AsSelf()
+                                      .InstancePerLifetimeScope();
+                       });
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
