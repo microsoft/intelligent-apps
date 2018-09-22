1a2d4ec05d4fefe293d8e2f7f33e1aad8559914a
wandrille-hubert <42714812+wandrille-hubert@users.noreply.github.com>
2018-09-21 21:40:58 -0500

    Pbi1 begin fabrikam updates (#31)
    * chatbot pbi1 begin updates
    
    * PBIBegin Fabrikam Updates
    
    * Revert "chatbot pbi1 begin updates"
    
    This reverts commit a89dad821c518c3b8257a6c944e2bf07a81e5df2.

--- FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
+++ FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.CognitiveServices.SpeechRecognition;
+﻿using Microsoft.CognitiveServices.Speech;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,62 +12,107 @@ namespace CallFabrikamCustomerService
     public partial class MainWindow : Window
     {
         //These are fields needed for using speech recognition client library aka. Project Oxford
-        private SpeechRecognitionMode Mode;
         private string DefaultLocale;
-        private MicrophoneRecognitionClient micClient;
         SoundPlayer thinking;
+        private TaskCompletionSource<int> stopBaseRecognitionTaskCompletionSource;
+        private SpeechRecognizer recognizer;
 
         private void StartMicrophone()
         {
-            //TODO: create and start mic recognition client
+            //TODO: create and start mic recognition
             
         }
 
         private void StopMicrophone()
         {
-            //TODO: end mic recognition and dispose of client
+            //TODO: end mic recognition and dispose of recognizer
 
         }
 
-        //Creates a new microphone client to start recoding and send send audio stream to Speech API automatically for text transcription
-        private void CreateMicrophoneRecoClient()
+        //Creates a new microphone recognizer to start recoding and send send audio stream to Speech API automatically for text transcription
+        private void CreateMicrophoneReco()
         {
             thinking = new SoundPlayer(@"../../Resources/SpeechResponse_Thinking.wav");
 
             //TODO: use the speech recognition factory to create mic client
 
 
+            //TODO: Call RunRecognizer Wire up Event handlers for speech recognition results
+
+        }
+
+        /// <summary>
+        /// Subscribes to Recognition Events
+        /// Starts the Recognition and waits until Final Result is received, then Stops recognition
+        /// </summary>
+        /// <param name="recognizer">Recognizer object</param>
+        /// <param name="recoType">Type of Recognizer</param>
+        ///  <value>
+        ///   <c>Base</c> if Baseline model; otherwise, <c>Custom</c>.
+        /// </value>
+        private async Task RunRecognizer(SpeechRecognizer recogniz, TaskCompletionSource<int> source)
+        {
+            recognizer = recogniz;
+
             //TODO: Wire up Event handlers for speech recognition results
 
+            //start,wait,stop recognition
+            await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+            await source.Task.ConfigureAwait(false);
+            await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+
+            //TODO: Unsubscribe from Event handlers
         }
 
-        //Event handler that is called when the microphone status is ready
-        private void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        #region Recognition Event Handlers
+
+        /// <summary>
+        /// Logs Intermediate Recognition results
+        /// </summary>
+        private void IntermediateResultEventHandler(SpeechRecognitionResultEventArgs e)
         {
-            //TODO: Writeline out mic status change to onscreen display
-            
+            //TODO: end recognition
         }
 
-        //This event handler gets called when full response audio is sent and transcribed
-        private void OnMicShortPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        /// <summary>
+        /// Logs the Final result
+        /// </summary>
+        private void FinalResultEventHandler(SpeechRecognitionResultEventArgs e)
         {
             thinking.PlaySync();
 
             //TODO: call EchoResponse to write out received response 
 
             //TODO: start mic recognition again
-
         }
 
-        //writes the error from Speech API if any to the onscreen display
-        private void OnConversationErrorHandler(object sender, SpeechErrorEventArgs e)
+        /// <summary>
+        /// Logs Error events
+        /// And sets the TaskCompletionSource to 0, in order to trigger Recognition Stop
+        /// </summary>
+        private void ErrorEventHandler(RecognitionErrorEventArgs e, TaskCompletionSource<int> source)
         {
+            this.WriteLine("--- Error received by ErrorEventHandler() ---");
+            source.TrySetResult(0);
             TransitionHangUpGui();
-
-            this.WriteLine("--- Error received by OnConversationErrorHandler() ---");
-            this.WriteLine("Error code: {0}", e.SpeechErrorCode.ToString());
-            this.WriteLine("Error text: {0}", e.SpeechErrorText);
-            this.WriteLine();
         }
+
+        /// <summary>
+        /// If SessionStoppedEvent is received, sets the TaskCompletionSource to 0, in order to trigger Recognition Stop
+        /// </summary>
+        private void SessionEventHandler(SessionEventArgs e, TaskCompletionSource<int> source)
+        {
+            if (e.EventType == SessionEventType.SessionStoppedEvent)
+            {
+                source.TrySetResult(0);
+            }
+        }
+
+        private void SpeechDetectedEventHandler(RecognitionEventArgs e)
+        {
+            //TODO: Writeline out mic status change to onscreen display
+        }
+
+        #endregion
     }
 }
