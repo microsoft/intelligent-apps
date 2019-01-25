d273f89bf638d6b16774e6e7585a4ac51ac103da
Wandrille Hubert <wahube@microsoft.com>
2019-01-25 16:40:08 -0600

    PBI2-Begin v4 Update
    PBI2-Begin v4 Update

---
+++ ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/App_Start/BotConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Hosting;
+using System.Web.Http;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.WebApi;
+using Microsoft.Bot.Configuration;
+using Unity;
+using Unity.Lifetime;
+
+namespace ContosoHelpdeskChatBot.App_Start
+{
+    public class BotConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapBotFramework(botConfig =>
+            {
+                // Load Connected Services from .bot file
+                var path = HostingEnvironment.MapPath(@"~/contosohelpdeskchatbot.bot");
+                var botConfigurationFile = BotConfiguration.Load(path);
+                var endpointService = (EndpointService)botConfigurationFile.Services.First(s => s.Type == "endpoint");
+
+                botConfig
+                    .UseMicrosoftApplicationIdentity(endpointService?.AppId, endpointService?.AppPassword);
+
+                // The Memory Storage used here is for local bot debugging only. When the bot
+                // is restarted, everything stored in memory will be gone.
+                IStorage dataStore = new MemoryStorage();
+
+                // Create Conversation State object.
+                // The Conversation State object is where we persist anything at the conversation-scope.
+                var conversationState = new ConversationState(dataStore);
+
+                // Create the custom state accessor.
+                // State accessors enable other components to read and write individual properties of state.
+                var accessors = new ConsotoChatBotAccessors(conversationState)
+                {
+                    //ContosoBotState = conversationState.CreateProperty<BotState>(ConsotoChatBotAccessors.CounterStateName),
+                };
+
+                UnityConfig.Container.RegisterInstance<ConsotoChatBotAccessors>(accessors, new ContainerControlledLifetimeManager());
+            });
+        }
+    }
+}
--- ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Dialogs/ResetPasswordDialog.cs
+++ ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Dialogs/ResetPasswordDialog.cs
@@ -1,16 +1,77 @@
-﻿namespace ContosoHelpdeskChatBot.Dialogs
-{
-    using System;
-    using System.Threading.Tasks;
-    using Microsoft.Bot.Builder.Dialogs;
-    using Microsoft.Bot.Connector;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using ContosoHelpdeskChatBot.Models;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.Bot.Builder;
 
+namespace ContosoHelpdeskChatBot.Dialogs
+{
     [Serializable]
-    public class ResetPasswordDialog : IDialog<object>
+    public class ResetPasswordDialog : WaterfallDialog
     {
-        public async Task StartAsync(IDialogContext context)
+        public static string dialogId = "ResetPasswordDialog";
+
+        public ResetPasswordDialog(string dialogId, IEnumerable<WaterfallStep> steps = null) : base(dialogId, steps)
         {
-            context.Fail(new NotImplementedException("Reset Password Dialog is not implemented and is instead being used to show context.Fail"));
+            AddStep(GreetingStepAsync);
+            AddStep(PasscodeReceivedStepAsync);
+        }
+
+
+        private async Task<DialogTurnResult> GreetingStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            await stepContext.Context.SendActivityAsync("Alright I will help you create a temp password");
+            if (sendPassCode(stepContext))
+            {
+                //TODO: prompt user
+                return null;
+            }
+            else
+            {
+                //TODO: notify user and end dialog
+                return null;
+            }
+        }
+
+        private async Task<DialogTurnResult> PasscodeReceivedStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            int result;
+            bool isNum = int.TryParse(stepContext.Context.Activity.Text, out result);
+            if (isNum)
+            {
+                var email = stepContext.Context.Activity.From.Id;
+                int? passcode;
+
+                //TODO: Lookup ResetPassword table and generate temporary password if pass code matches
+                return null;
+            }
+            else
+            {
+                await stepContext.Context.SendActivityAsync($"Invalid passcode.");
+                return await stepContext.EndDialogAsync();
+            }
+        }
+
+        private bool sendPassCode(WaterfallStepContext context)
+        {
+            bool result = false;
+
+            //TODO: Use context.Activity.From.Id to match email stored in database
+            //From Id varies depending on channel
+            //refer ChannelAccount class https://docs.botframework.com/en-us/csharp/builder/sdkreference/dd/def/class_microsoft_1_1_bot_1_1_connector_1_1_channel_account.html#a0b89cf01fdd73cbc00a524dce9e2ad1a
+            //as well as Activity class https://docs.botframework.com/en-us/csharp/builder/sdkreference/dc/d2f/class_microsoft_1_1_bot_1_1_connector_1_1_activity.html
+
+
+            //TODO: Save PassCode to database
+
+
+            //TODO: Send SMS to mobile number looked up from ResetPassword table
+
+            return result;
         }
     }
 }
