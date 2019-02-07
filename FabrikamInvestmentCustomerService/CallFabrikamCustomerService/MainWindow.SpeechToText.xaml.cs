ca6ae0eed6a064f618d1d14c5839c83d786697f6
Wandrille Hubert <wahube@microsoft.com>
2019-02-06 18:36:57 -0600

    pbi1-begin cleaningup
    pbi1-begin cleaningup

--- FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
+++ FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.CognitiveServices.Speech;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Media;
-using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
