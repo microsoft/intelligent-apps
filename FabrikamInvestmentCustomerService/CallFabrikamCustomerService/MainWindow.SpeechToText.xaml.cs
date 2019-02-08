b5ddc21d919f1c1d2136c05b333bbce824308c35
Wandrille Hubert <wahube@microsoft.com>
2019-02-08 13:57:06 -0600

    updates

--- FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
+++ FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
@@ -51,10 +51,11 @@ namespace CallFabrikamCustomerService
             string path = Assembly.GetExecutingAssembly().Location;
             string path1 = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), "Resources\\SpeechResponse_Thinking.wav");
             thinking = new SoundPlayer(path1);
-            // Todo: suport users to specifiy a different region.
+
             try
             {
                 var speechConfig = SpeechConfig.FromSubscription(this.MicrosoftSpeechApiKey, this.Region);
+                speechConfig.SpeechRecognitionLanguage = this.DefaultLocale;
 
                 SpeechRecognizer basicRecognizer;
 
@@ -65,7 +66,8 @@ namespace CallFabrikamCustomerService
             }
             catch (Exception ex)
             {
-
+                WriteLine($"An exception occured:{ex}");
+                Console.WriteLine($"An exception occured:{ex}");
             }
         }
 
