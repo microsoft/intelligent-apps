df0c09dc0836a38c26b672690dfaf0f80eb3f38c
Wandrille Hubert <wahube@microsoft.com>
2019-02-08 13:45:01 -0600

    Update MainWindow.SpeechToText.xaml.cs

--- FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
+++ FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.SpeechToText.xaml.cs
@@ -40,7 +40,7 @@ namespace CallFabrikamCustomerService
 
         /// <summary>
         /// Creates Recognizer with English language and microphone
-        /// Creates a factory with subscription key and selected region
+        /// Creates a config with subscription key and selected region
         /// Waits on RunRecognition
         /// </summary>
         private async Task CreateMicrophoneReco()
@@ -48,7 +48,7 @@ namespace CallFabrikamCustomerService
             string path = Assembly.GetExecutingAssembly().Location;
             string path1 = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(path), "Resources\\SpeechResponse_Thinking.wav");
             thinking = new SoundPlayer(path1);
-            // Todo: suport users to specifiy a different region.
+
             try
             {
                 var speechConfig = SpeechConfig.FromSubscription(this.MicrosoftSpeechApiKey, this.Region);
