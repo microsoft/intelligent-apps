360a8ff4635b9ffc019d765d23db7a1bf6e128f9
Wandrille Hubert <wahube@microsoft.com>
2019-02-08 15:17:53 -0600

    Update MainWindow.TextToSpeech.xaml.cs

--- FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.TextToSpeech.xaml.cs
+++ FabrikamInvestmentCustomerService/CallFabrikamCustomerService/MainWindow.TextToSpeech.xaml.cs
@@ -182,19 +182,5 @@ namespace CallFabrikamCustomerService
                 }
             }
         }
-
-        private async Task<string> FetchTokenAsync(string fetchUri, string subscriptionKey)
-        {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
-                UriBuilder uriBuilder = new UriBuilder(fetchUri);
-
-                var result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null);
-                Console.WriteLine("Token Uri: {0}", uriBuilder.Uri.AbsoluteUri);
-                return await result.Content.ReadAsStringAsync();
-            }
-        }
-
     }
 }
