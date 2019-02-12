c5b84c5488da4ab81cecd6baf0e887f590d132d2
wandrille-hubert <42714812+wandrille-hubert@users.noreply.github.com>
2019-02-12 12:55:38 -0600

    Feb2019Update_ContosoHelpdeskChatBot_Pbi2 begin v4 Update (#43)
    * pni2begin fabrikam updates
    
    * pbi2begin fabrikam updates
    
    * PBI2-Begin v4 Update
    
    PBI2-Begin v4 Update
    
    * Update MainDialog.cs
    
    * Update InstallAppDialog.cs

--- ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Global.asax.cs
+++ ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Global.asax.cs
@@ -1,7 +1,5 @@
 ﻿using Autofac;
 using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Dialogs.Internals;
-using Microsoft.Bot.Builder.Internals.Fibers;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Azure;
 using System;
@@ -11,6 +9,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using ContosoHelpdeskChatBot.App_Start;
+
 
 namespace ContosoHelpdeskChatBot
 {
@@ -18,40 +18,13 @@ namespace ContosoHelpdeskChatBot
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-
-            BotConfig.UpdateConversationContainer();
-
-            //TODO: Uncomment after adding log4net Nuget package 
-            //log4net.Config.XmlConfigurator.Configure();
-        }
-
-        //setting Bot data store policy to use last write win
-        //example if bot service got restarted, existing conversation would just overwrite data to store
-        public static class BotConfig
-        {
-            public static void UpdateConversationContainer()
+            GlobalConfiguration.Configure(config =>
             {
-                var store = new InMemoryDataStore();
+                BotConfig.Register(config);
+            });
 
-                Conversation.UpdateContainer(
-                           builder =>
-                           {
-                               builder.Register(c => store)
-                                         .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
-                                         .AsSelf()
-                                         .SingleInstance();
-
-                               builder.Register(c => new CachingBotDataStore(store,
-                                          CachingBotDataStoreConsistencyPolicy
-                                          .ETagBasedConsistency))
-                                          .As<IBotDataStore<BotData>>()
-                                          .AsSelf()
-                                          .InstancePerLifetimeScope();
-
-
-                           });
-            }
+            //TODO: Uncomment after adding log4net Nuget package
+            //log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
