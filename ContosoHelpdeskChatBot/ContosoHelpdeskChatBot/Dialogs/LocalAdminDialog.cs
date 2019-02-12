7889b68025693550b479b6d7ed28d386816f6c33
wandrille-hubert <42714812+wandrille-hubert@users.noreply.github.com>
2019-02-12 12:54:36 -0600

    Feb2019Update_ContosoHelpdeskChatBot_PBI4 End Update to v4 (master) (#40)
    * PBI4 End Updates
    
    PBI4 End Updates
    
    * pbi4end fabrikam updates
    
    * pbi4end fabrikam updates
    
    * v4 update
    
    v4 update
    
    * updates and cleaning
    
    * Update MainDialog.cs
    
    cleanup

--- ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Dialogs/LocalAdminDialog.cs
+++ ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Dialogs/LocalAdminDialog.cs
@@ -3,58 +3,65 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using ContosoHelpdeskChatBot.Models;
-using Microsoft.Bot.Builder.FormFlow;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder;
+using System.Threading;
 
 namespace ContosoHelpdeskChatBot.Dialogs
 {
     [Serializable]
-    public class LocalAdminDialog : IDialog<object>
+    public class LocalAdminDialog : WaterfallDialog
     {
         private LocalAdmin admin = new LocalAdmin();
-        public async Task StartAsync(IDialogContext context)
+        public static string dialogId = "LocalAdminDialog";
+
+        public LocalAdminDialog(string dialogId, IEnumerable<WaterfallStep> steps = null) : base(dialogId, steps)
         {
-            await context.PostAsync("Great I will help you request local machine admin.");
-
-            var localAdminDialog = FormDialog.FromForm(this.BuildLocalAdminForm, FormOptions.PromptInStart);
-
-            context.Call(localAdminDialog, this.ResumeAfterLocalAdminFormDialog);
+            AddStep(GreetingStepAsync);
+            AddStep(ResponseConfirmStepAsync);
+            AddStep(finalStepAsync);
         }
 
-        private async Task ResumeAfterLocalAdminFormDialog(IDialogContext context, IAwaitable<LocalAdminPrompt> userReply)
+
+        private static async Task<DialogTurnResult> GreetingStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            
-            using (var db = new ContosoHelpdeskContext())
+            await stepContext.Context.SendActivityAsync($"Great! I will help you request local machine admin.");
+            return await stepContext.PromptAsync("promptText", new PromptOptions { Prompt = MessageFactory.Text("What is the machine name to add you to local admin group?") }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ResponseConfirmStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var MachineName = (string)stepContext.Result;
+            stepContext.Values["MachineName"] = MachineName;
+
+            return await stepContext.PromptAsync("promptNumber", new PromptOptions { Prompt = MessageFactory.Text("How many days do you need the admin access?") }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> finalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            int days = (int)stepContext.Result;
+
+            if (days >= 0)
             {
-                db.LocalAdmins.Add(admin);
-                db.SaveChanges();
+                stepContext.Values["AdminDuration"] = days;
+                admin.AdminDuration = (int)stepContext.Values["AdminDuration"];
+                admin.MachineName = (string)stepContext.Values["MachineName"];
+
+                using (var db = new ContosoHelpdeskContext())
+                {
+                    db.LocalAdmins.Add(admin);
+                    db.SaveChanges();
+                }
+
+                var ticketNumber = new Random().Next(0, 20000);
+                await stepContext.Context.SendActivityAsync($"Thank you for using the Helpdesk Bot. Your ticket number is {ticketNumber}.");
+                return await stepContext.EndDialogAsync();
             }
-
-            context.Done<object>(null);
-        }
-
-        private IForm<LocalAdminPrompt> BuildLocalAdminForm()
-        {
-            //here's an example of how validation can be used in form builder
-            return new FormBuilder<LocalAdminPrompt>()
-                .Field(nameof(LocalAdminPrompt.MachineName),
-                validate: async (state, value) =>
-                {
-                    var result = new ValidateResult { IsValid = true, Value = value };
-                    //add validation here
-
-                    this.admin.MachineName = (string)value;
-                    return result;
-                })
-                .Field(nameof(LocalAdminPrompt.AdminDuration),
-                validate: async (state, value) =>
-                {
-                    var result = new ValidateResult { IsValid = true, Value = value };
-                    //add validation here
-
-                    this.admin.AdminDuration = Convert.ToInt32((long)value) as int?;
-                    return result;
-                })
-                .Build();
+            else
+            {
+                await stepContext.Context.SendActivityAsync($"Invalid response provided.  Ending dialog.");
+                return await stepContext.EndDialogAsync();
+            }
         }
     }
 }
--- ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Global.asax.cs
+++ ContosoHelpdeskChatBot/ContosoHelpdeskChatBot/Global.asax.cs
@@ -1,7 +1,5 @@
 ﻿using Autofac;
 using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Dialogs.Internals;
-using Microsoft.Bot.Builder.Internals.Fibers;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Azure;
 using System;
@@ -11,6 +9,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using ContosoHelpdeskChatBot.App_Start;
+
 
 namespace ContosoHelpdeskChatBot
 {
@@ -18,49 +18,12 @@ namespace ContosoHelpdeskChatBot
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-
-            BotConfig.UpdateConversationContainer();
-            this.RegisterBotModules();
+            GlobalConfiguration.Configure(config =>
+            {
+                BotConfig.Register(config);
+            });
 
             log4net.Config.XmlConfigurator.Configure();
-        }
-
-        //setting Bot data store policy to use last write win
-        //example if bot service got restarted, existing conversation would just overwrite data to store
-        public static class BotConfig
-        {
-            public static void UpdateConversationContainer()
-            {
-                var store = new InMemoryDataStore();
-
-                Conversation.UpdateContainer(
-                           builder =>
-                           {
-                               builder.Register(c => store)
-                                         .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
-                                         .AsSelf()
-                                         .SingleInstance();
-
-                               builder.Register(c => new CachingBotDataStore(store,
-                                          CachingBotDataStoreConsistencyPolicy
-                                          .ETagBasedConsistency))
-                                          .As<IBotDataStore<BotData>>()
-                                          .AsSelf()
-                                          .InstancePerLifetimeScope();
-
-
-                           });
-            }
-        }
-
-        private void RegisterBotModules()
-        {
-            Conversation.UpdateContainer(builder =>
-            {
-                builder.RegisterModule(new ReflectionSurrogateModule());
-                builder.RegisterModule<GlobalMessageHandlersBotModule>();
-            });
         }
     }
 }
