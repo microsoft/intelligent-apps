10bdf5df02994ae878990418b879e0961bf6bb5d
Leo Leong <leoleo@microsoft.com>
2019-02-22 20:59:59 -0800

    bugfixes (#71)

--- ContosoHelpdeskChatBot/ContosoHelpdeskSms/Helper.cs
+++ ContosoHelpdeskChatBot/ContosoHelpdeskSms/Helper.cs
@@ -13,14 +13,14 @@ namespace ContosoHelpdeskSms
     {
         public static bool SendSms(string ToMobileNumber, string Message)
         {
-            string TWILIO_ACCOUNT_SID = "AC724d44f065c6423925f36597c2351b2b";
-            string TWILIO_AUTH_TOKEN = "5804c1c451d19a2f14843cc9735294f6";
+            string TWILIO_ACCOUNT_SID = "Replace with your Twilio account SID";
+            string TWILIO_AUTH_TOKEN = "Replace with your auth token";
 
             TwilioClient.Init(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
             var message = MessageResource.Create(
                 to: new PhoneNumber(ToMobileNumber),
-                from: new PhoneNumber("+12062080995"),
+                from: new PhoneNumber("+TwilioTrialNumber"),
                 body: Message
                 );
 
