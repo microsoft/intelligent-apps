41b6190fbdb2b2de2716d2aab8e5e0cad9504a95
wandrille-hubert <42714812+wandrille-hubert@users.noreply.github.com>
2019-02-25 13:14:29 -0600

    Feb2019Update_FabrikamInvestmentCustomerService_PBI3-End Bot v4 Update And Speech v1.2 (#54)
    * PBI3 End Updates
    
    PBI3 End Updates
    
    * PBI3 End Updates
    
    PBI3 End Updates
    
    * Revert "PBI3 End Updates"
    
    This reverts commit a39a84106383adeb3aaa494b730a5ba2e4bbb216.
    
    * Revert "PBI3 End Updates"
    
    This reverts commit d4215f684c34e6965427902bb985f91b2a781b80.
    
    * PBI3-End Chat Bot Updates
    
    PBI3-End Chat Bot Updates
    
    * fab pbi3-end v4 update
    
    fab pbi3-end v4 update
    
    * Update CallFabrikamCustomerService.csproj
    
    * Update MainWindow.xaml.cs
    
    * updates
    
    * Update MainWindow.SpeechToText.xaml.cs
    
    * removed bot.builder.azure
    
    * Update MainWindow.TextToSpeech.xaml.cs
    
    * Update MainWindow.SpeechToText.xaml.cs
    
    * Update MainWindow.TextToSpeech.xaml.cs

---
+++ FabrikamInvestmentCustomerService/FabrikamCustomerServiceBot/FabrikamServiceBotAccessors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace FabrikamCustomerServiceBot
+{
+    public class FabrikamServiceBotAccessors
+    {
+        public FabrikamServiceBotAccessors(ConversationState conversationState)
+        {
+            ConversationState = conversationState ?? throw new ArgumentNullException(nameof(conversationState));
+        }
+
+        public ConversationState ConversationState { get; }
+    }
+}
