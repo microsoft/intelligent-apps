8feb83b632e5359c5759c2d5820b3e6f2df7f559
Leo Leong <leoleo@microsoft.com>
2019-05-01 15:04:49 -0700

    removing code in docs branch

--- AdatumTaxCorpKnowledgeService/AdatumTaxCorpKnowledgeData/dbo/Tables/Faqs.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Faqs] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [Question] NVARCHAR (MAX) NULL,
-    [Answer]   NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Faqs] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
